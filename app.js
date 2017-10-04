0f13174ef4b9b43d8878084af37d3df25e2f7aea
Dario <darioarizabalo@gmail.com>
2017-10-03 21:19:44 -0300

    cambio el orden

--- app.js
+++ app.js
@@ -80,40 +80,6 @@ app.get('/webhook', function(req, res) {
     }
   }
 
-  function sendTextMessage(recipientId, messageText) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: messageText
-      }
-    };
-  
-    callSendAPI(messageData);
-  }
-
-  function callSendAPI(messageData) {
-    request({
-      uri: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-      method: 'POST',
-      json: messageData
-  
-    }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var recipientId = body.recipient_id;
-        var messageId = body.message_id;
-  
-        console.log("Successfully sent generic message with id %s to recipient %s", 
-          messageId, recipientId);
-      } else {
-        console.error("Unable to send message.");
-        console.error(response);
-        console.error(error);
-      }
-    });  
-  }
 
   function sendGenericMessage(recipientId) {
     var messageData = {
@@ -162,6 +128,40 @@ app.get('/webhook', function(req, res) {
     callSendAPI(messageData);
   }
 
+  function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
+      }
+    };
+  
+    callSendAPI(messageData);
+  }
+
+  function callSendAPI(messageData) {
+    request({
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: messageData
+  
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var recipientId = body.recipient_id;
+        var messageId = body.message_id;
+  
+        console.log("Successfully sent generic message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+        console.error("Unable to send message.");
+        console.error(response);
+        console.error(error);
+      }
+    });  
+  }
   function receivedPostback(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
